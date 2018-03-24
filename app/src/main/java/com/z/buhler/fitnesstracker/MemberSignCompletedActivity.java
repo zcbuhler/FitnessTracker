20e793a66e1acf5c02977246c2a7d5e2eb26b859
zcbuhler <zcbuhler@gmail.com>
2018-03-24 12:21:39 -0400

    Initial commit, back-up before login activity replacement

---
+++ app/src/main/java/com/z/buhler/fitnesstracker/MemberSignCompletedActivity.java
@@ -0,0 +1,13 @@
+package com.z.buhler.fitnesstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemberSignCompletedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_sign_completed);
+    }
+}
